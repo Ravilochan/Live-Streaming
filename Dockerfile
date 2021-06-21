f198878b046bd36f74e3d649b2304cbd7689b93b
Ravilochan <ravilochan2000@gmail.com>
2021-06-21 05:49:28 +0530

    Live Stream

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
 FROM tiangolo/nginx-rtmp
 
 COPY nginx.conf /etc/nginx/nginx.conf
+
 COPY index.html /www/
+
+EXPOSE 1935 8080
--- stream.sh
+++ stream.sh
@@ -1,1 +1,1 @@
-ffmpeg -re -i bbb_sunflower_1080p_60fps_normal.mp4 -vcodec copy -loop -1 -c:a aac -b:a 160k -ar 44100 -strict -2 -f flv rtmp://<ingest-node-ip>/live/bbb
+ffmpeg -re -i Race.mp4 -vcodec copy -loop -1 -c:a aac -b:a 128k -ar 44100 -strict -2 -f flv rtmp://test2.statledger.io/live/test
