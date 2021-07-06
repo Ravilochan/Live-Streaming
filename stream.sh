966084e3aed343e754593981f129edd9664fe9c2
RaviLochan <ravilochan2000@gmail.com>
2021-07-06 18:37:57 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,1 +1,1 @@
-ffmpeg -re -i Race.mp4 -vcodec copy -loop -1 -c:a aac -b:a 128k -ar 44100 -strict -2 -f flv rtmp://test2.statledger.io/live/test
+ffmpeg -re -i Race.mp4 -vcodec copy -loop -1 -c:a aac -b:a 128k -ar 44100 -strict -2 -f flv rtmp://<<URL>>/live/test
